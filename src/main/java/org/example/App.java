05b5e50865d7779f559f44c6039878979552e8bb
Haslam <haslamhdz71@gmail.com>
2024-03-06 11:24:27 +0700

    init: membuat inisialisasi running

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,7 +7,7 @@ import org.example.Service.RegionService;
 import org.example.Utility.DatabaseConnection;
 import org.example.Utility.FactoryPropertiesReader;
 import org.example.Utility.PropertiesReader;
-import org.example.View.HomeView;
+import org.example.View.MainView;
 import org.example.View.RegionView;
 
 
@@ -17,10 +17,12 @@ public class App
         PropertiesReader propertiesDatabase      = FactoryPropertiesReader.databasePropDefault();
         DatabaseConnection databaseConfiguration = new DatabaseConnection(propertiesDatabase);
         RegionRepository regionRepository        = new RegionImpl(databaseConfiguration);
-        RegionService regionService = new RegionService((RegionImpl) regionRepository);
-        RegionController regionController        = new RegionController(regionService);
-        RegionView regionView                    = new RegionView(regionController);
-        HomeView homeView                        = new HomeView(regionView);
-        homeView.start();
+
+        RegionController regionController = new RegionController(regionRepository);
+        RegionView regionView = new RegionView(regionController);
+        MainView mainView = new MainView(regionView);
+
+        mainView.start();
+
     }
 }
