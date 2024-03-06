2b716536b6f4f5cbdd2efd041f97846bbe8615de
Haslam <haslamhdz71@gmail.com>
2024-03-06 11:22:32 +0700

    add : menambahkan controller,view,main

--- src/main/java/org/example/View/RegionView.java
+++ src/main/java/org/example/View/RegionView.java
@@ -6,36 +6,59 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class RegionView {
-    final String[] menus =
-            { "List Region", "Tambah Region"
-            ,"Cari Region dengan nama", "Ubah nama region"
-            ,"Hapus region","Keluar"};
-    protected boolean status = true;
     protected RegionController regionController;
     private Scanner scanner = new Scanner(System.in);
+
+    public RegionView() {
+    }
 
     public RegionView(RegionController regionController) {
         this.regionController = regionController;
     }
 
-
-
     public void deleteRegion() {
+        System.out.println("-------------------------");
+        System.out.println("------Hapus region------");
+        System.out.print("Input angka urutan region : ");
+
+        int input = scanner.nextInt();
+
+        regionController.deleteRegion(input);
     }
 
     public void updateRegionName() {
+        System.out.println("-------------------------");
+        System.out.println("------Update Nama Region------");
+        System.out.print("Input angka urutan region : ");
+        int inputUrutan = scanner.nextInt();
+        System.out.print("Input nama baru region : ");
+        String inputName = scanner.next();
+
+        regionController.changeNameRegion(inputUrutan,inputName);
     }
 
     public void findRegion() {
+        System.out.println("-------------------------");
+        System.out.println("------Cari Region------");
+        System.out.print("Input nama region : ");
+        String inputName = scanner.next();
+
+        regionController.findRegionByName(inputName);
     }
 
-    public void insertingRegionView() {
+    public void addRegion() {
+        System.out.println("-------------------------");
+        System.out.println("------Tambah region------");
+        System.out.print("Input Nama : ");
+
+        String nameInput = scanner.nextLine();
+
+        regionController.addRegion(nameInput);
     }
 
     public void regionListView() {
         System.out.println("-------------------------");
         System.out.println("-------List Region-------");
-        System.out.println("-------------------------");
         regionController.listRegion();
         System.out.println("-------------------------");
     }
